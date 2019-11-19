732882272c52b430a37bd2dbb2a144541a781f48
赫敏璋 <loongsking@hotmail.com>
2019-11-19 23:13:53 +0800

    add c# 6.0 Read-only auto-properties Demo

---
+++ 6/Read-only auto-properties/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public Student(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException(message: "Cannot be blank", paramName: nameof(lastName));
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public void ChangeName(string newLastName)
+        {
+            // Generates CS0200: Property or indexer cannot be assigned to -- it is read only
+            LastName = newLastName;
+        }
+    }
+}
