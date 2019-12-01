1464e82f0c33e74e6f5ab1689033464b5bf1d33a
赫敏璋 <loongsking@hotmail.com>
2019-12-01 23:24:21 +0800

    update c# 6.0 Read-only auto-properties Demo

--- 06/Read-only auto-properties/Student.cs
+++ 06/Read-only auto-properties/Student.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
     {
         public string FirstName { get; }
         public string LastName { get; }
+        //public string FullName { get; } => $"{FirstName}, {LastName}";
+        public string FullName => $"{FirstName}, {LastName}";
         public Student(string firstName, string lastName)
         {
             if (string.IsNullOrWhiteSpace(lastName))
---
+++ 06/Read-only auto-properties/StudentOlder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class StudentOlder_01
+    {
+        private string _firstName;
+        public string FirstName { get { return _firstName; } }
+        private string _lastName;
+        public string LastName { get { return _lastName; } }
+        public StudentOlder_01(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException(message: "Cannot be blank", paramName: nameof(lastName));
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+    }
+    public class StudentOlder_02
+    {
+        private readonly string _firstName;
+        public string FirstName { get { return _firstName; } }
+        private readonly string _lastName;
+        public string LastName { get { return _lastName; } }
+        public StudentOlder_02(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException(message: "Cannot be blank", paramName: nameof(lastName));
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+    }
+}
